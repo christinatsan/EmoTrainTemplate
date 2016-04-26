25b411ce8f41a3db9d03e9e6ac7f003e028f054a
christinatsan <ctsango000@citymail.cuny.edu>
2016-04-26 12:04:25 -0400

    Initial Commit

---
+++ EmoTrainTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmoTrainTemplate
+//
+//  Created by Christina Tsangouri on 4/26/16.
+//  Copyright © 2016 Christina Tsangouri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
